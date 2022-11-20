b8a6e8408d49718124ab84f3d6ad772e4ba9e6e9
rajibdas-it <rajibrad@gmail.com>
2022-11-20 17:59:57 +0600

    make admin function not work

--- src/Pages/Dashboard/AllUser/AllUser.js
+++ src/Pages/Dashboard/AllUser/AllUser.js
@@ -2,11 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const AllUser = () => {
-  const { data: users = [] } = useQuery({
-    queryKey: ["allusers"],
+  const { data: users = [], refetch } = useQuery({
+    queryKey: ["users"],
     queryFn: () =>
       fetch("http://localhost:5000/users").then((res) => res.json()),
   });
+
+  const handleMakeAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+        }
+      });
+  };
   return (
     <div>
       <h1>Show all users</h1>
@@ -18,8 +30,9 @@ const AllUser = () => {
               <tr>
                 <th></th>
                 <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
+                <th>Email</th>
+                <th>Admin</th>
+                <th>Delete</th>
               </tr>
             </thead>
             <tbody>
@@ -28,7 +41,19 @@ const AllUser = () => {
                   <th>{i + 1}</th>
                   <td>{user.name}</td>
                   <td>{user.email}</td>
-                  <td>Blue</td>
+                  <td>
+                    {user.role !== "admin" && (
+                      <button
+                        onClick={() => handleMakeAdmin(user._id)}
+                        className="btn btn-xs btn-primary"
+                      >
+                        Make Admin
+                      </button>
+                    )}
+                  </td>
+                  <td>
+                    <button className="btn btn-xs btn-danger">Delete</button>
+                  </td>
                 </tr>
               ))}
             </tbody>
